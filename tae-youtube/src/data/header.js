a418c0f8927f98c3062d12cc23bab6e67d1cae34
TaekyeongOhMe <161999984+TaekyeongOh@users.noreply.github.com>
2024-07-09 09:44:32 +0900

    헤더 영역 데이터 작업

---
+++ tae-youtube/src/data/header.js
@@ -0,0 +1,95 @@
+import { AiFillGithub } from "react-icons/ai";
+import { AiOutlineInstagram } from "react-icons/ai";
+import { SiVelog } from "react-icons/si";
+export const headerMenus = [
+    {
+        title: "태경태세문단세",
+        src: "/"
+    },
+    {
+        title: "키스 오브 라이프",
+        src: "/today"
+    },
+    {
+        title: "뉴진스",
+        src: "/developer"
+    },
+    {
+        title: "에스파",
+        src: "/webd"
+    },
+    {
+        title: "블랙핑크",
+        src: "/website"
+    },
+    {
+        title: "레드벨벳",
+        src: "/gsap"
+    },
+    
+];
+
+export const searchKeyword = [
+    {
+        title: "webstoryboy",
+        src: "/search/webstoryboy"
+    },
+    {
+        title: "HTML",
+        src: "/search/html"
+    },
+    {
+        title: "CSS",
+        src: "/search/css"
+    },
+    {
+        title: "JavaScript",
+        src: "/search/javascript"
+    },
+    {
+        title: "React.js",
+        src: "/search/react.js"
+    },
+    {
+        title: "Vue.js",
+        src: "/search/vue.js"
+    },
+    {
+        title: "Next.js",
+        src: "/search/next.js"
+    },
+    {
+        title: "Node.js",
+        src: "/search/node.js"
+    },
+    {
+        title: "SQL",
+        src: "/search/sql"
+    },
+    {
+        title: "portfolio",
+        src: "/search/React Portfolio"
+    },
+    {
+        title: "music",
+        src: "/search/NewJeans"
+    }
+];
+
+export const snsLink = [
+    {
+        title: "github",
+        url: "https://github.com/TaekyeongOh",
+        icon: <AiFillGithub />
+    },
+    {
+        title: "instagram",
+        url: "https://www.instagram.com/t_aekyeong/",
+        icon: <AiOutlineInstagram />
+    },
+    {
+        title: "velog",
+        url: "https://velog.io/@tae_me/posts",
+        icon: <SiVelog />
+    }
+]
