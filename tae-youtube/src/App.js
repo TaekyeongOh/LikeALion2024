449f0b66fe51c2e167949636fa74f7be279115f2
TaekyeongOhMe <161999984+TaekyeongOh@users.noreply.github.com>
2024-07-09 10:53:20 +0900

    컴퍼넌트 비동기 작업

--- tae-youtube/src/App.js
+++ tae-youtube/src/App.js
@@ -1,25 +1,25 @@
-import React from 'react'
+import React,{Suspense,lazy} from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import Home from './pages/Home'
-import Today from './pages/Today'
-import Developer from './pages/Developer'
-import Webd from './pages/Webd'
-import Website from './pages/Website'
-import Gsap from './pages/Gsap'
-import Channel from './pages/Channel'
-import Video from './pages/Video'
-import Search from './pages/Search'
-import Not from './pages/Not'
-import Header from './components/section/Header';
 import Main from './components/section/Main';
 import Footer from './components/section/Footer';
+import Header from './components/section/Header';
+
+const Home = lazy(() => import('./pages/Home'));
+const Today = lazy(() => import('./pages/Today'));
+const Developer = lazy(() => import('./pages/Developer'));
+const Webd = lazy(() => import('./pages/Webd'));
+const Website = lazy(() => import('./pages/Website'));
+const Gsap = lazy(() => import('./pages/Gsap'));
+const Channel = lazy(() => import('./pages/Channel'));
+const Video = lazy(() => import('./pages/Video'));
+const Search = lazy(() => import('./pages/Search'));
+const Not = lazy(() => import('./pages/Not'));
 
 const App = () => {
     return (
         <BrowserRouter>
             <Header />
-            <Main>
+            <Suspense fallback={<Main/>}>
                 <Routes>
                     <Route path='/' element={<Home />} />
                     <Route path='/today' element={<Today />} />
@@ -32,7 +32,7 @@ const App = () => {
                     <Route path='/search/:searchId' element={<Search />} />
                     <Route path='*' element={<Not />} />
                 </Routes>
-            </Main>
+            </Suspense>
             <Footer />
         </BrowserRouter>
     );
