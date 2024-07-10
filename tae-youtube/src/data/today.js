03e4e1e2a037143c2462821d6cb43a6e3a10a454
TaekyeongOhMe <161999984+TaekyeongOh@users.noreply.github.com>
2024-07-10 10:36:54 +0900

    키스 오브 라이프 추천영상

---
+++ tae-youtube/src/data/today.js
@@ -0,0 +1,76 @@
+import todayView01 from "../assets/img/today/todayView01.jpg";
+import todayView02 from "../assets/img/today/todayView02.jpg";
+import todayView03 from "../assets/img/today/todayView03.jpg";
+import todayView04 from "../assets/img/today/todayView04.jpg";
+import todayView05 from "../assets/img/today/todayView05.jpg";
+import todayView06 from "../assets/img/today/todayView06.jpg";
+import todayView07 from "../assets/img/today/todayView07.jpg";
+import todayView08 from "../assets/img/today/todayView08.jpg";
+
+export const todayText = [
+    {
+        img: todayView01,
+        title: "키스 오브 라이프",
+        desc : "kiss",
+        author : "of",
+        channelId: "life",
+        date: "2023.07.07",
+        page: "/youtube"
+    },{
+        img: todayView02,
+        title: "키스 오브 라이프",
+        desc : "kiss",
+        author : "of",
+        channelId: "life",
+        date: "2023.07.07",
+        page: "/port"
+    },{
+        img: todayView03,
+        title: "키스 오브 라이프",
+        desc : "kiss",
+        author : "of",
+        channelId: "life",
+        date: "2023.07.07",
+        page: "/port"
+    },{
+        img: todayView04,
+        title: "키스 오브 라이프",
+        desc : "kiss",
+        author : "of",
+        channelId: "life",
+        date: "2023.07.07",
+        page: "/port"
+    },{
+        img: todayView05,
+        title: "키스 오브 라이프",
+        desc : "kiss",
+        author : "of",
+        channelId: "life",
+        date: "2023.07.07",
+        page: "/port"
+    },{
+        img: todayView06,
+        title: "키스 오브 라이프",
+        desc : "kiss",
+        author : "of",
+        channelId: "life",
+        date: "2023.07.07",
+        page: "/gsap"
+    },{
+        img: todayView07,
+        title: "키스 오브 라이프",
+        desc : "kiss",
+        author : "of",
+        channelId: "life",
+        date: "2023.07.07",
+        page: "/webd"
+    },{
+        img: todayView08,
+        title: "키스 오브 라이프",
+        desc : "kiss",
+        author : "of",
+        channelId: "life",
+        date: "2023.07.07",
+        page: "/website"
+    },
+]
