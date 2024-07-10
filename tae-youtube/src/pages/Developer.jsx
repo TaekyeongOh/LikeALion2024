3fb724329a263c015e6eb4eb4d965a1a21f1e14b
TaekyeongOhMe <161999984+TaekyeongOh@users.noreply.github.com>
2024-07-10 11:02:17 +0900

    뉴진스

--- tae-youtube/src/pages/Developer.jsx
+++ tae-youtube/src/pages/Developer.jsx
@@ -1,15 +1,36 @@
 import React from 'react'
 import Main from '../components/section/Main'
 
+import { developerText } from '../data/developer'
+import { Link } from 'react-router-dom'
+
 const Developer = () => {
-  return (
-    <Main
-    title='뉴진스'
-    description="new jeans"
-    >
-      Developer
-    </Main>
-  )
+    return (
+        <Main 
+            title = "뉴뉴"
+            description="진스">
+            
+            <section id='developerPage'>
+                <h2>뉴진 스</h2>
+                <div className="developer__inner">
+                    {developerText.map((developer, key) => (
+                        <div className="developer" key={key}>
+                            <div className="developer__img play__icon">
+                                <Link to={`/channel/${developer.channelId}`}>
+                                    <img src={developer.img} alt={developer.name} />
+                                </Link>
+                            </div>
+                            <div className="developer__info">
+                                <Link to={`/channel/${developer.channelId}`}>
+                                    {developer.name}
+                                </Link>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </section>
+        </Main>
+    )
 }
 
 export default Developer
