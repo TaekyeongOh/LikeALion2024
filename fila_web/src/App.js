95d8ad804570240d592419487d94002ecf09affc
TaekyeongOhMe <161999984+TaekyeongOh@users.noreply.github.com>
2024-07-12 22:36:39 +0900

    페이지 완성

--- fila_web/src/App.js
+++ fila_web/src/App.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home'
+import Women from './pages/Women'
+import Men from './pages/Men'
+import Kids from './pages/Kids'
+import Tennis from './pages/Tennis'
+import Brand from './pages/Brand'
+import Not from './pages/Not'
+
+// import Header from './components/section/Header'; 
+// import Main from './components/section/Main';   
+// import Footer from './components/section/Footer'; 
 
 const App = () => {
     return (
-        <div>App</div>
+        <BrowserRouter>
+            {/* <Header />
+            <Main> */}
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/women' element={<Women />} />
+                    <Route path='/men' element={<Men />} />
+                    <Route path='/kids' element={<Kids />} />
+                    <Route path='/tennis' element={<Tennis />} />
+                    <Route path='/brand' element={<Brand />} />
+                    <Route path='*' element={<Not />} />
+                </Routes>
+            {/* </Main>
+            <Footer /> */}
+        </BrowserRouter>
     )
 }
 
