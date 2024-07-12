52539b6edefc883185f9857adec51c8f48d59db8
TaekyeongOhMe <161999984+TaekyeongOh@users.noreply.github.com>
2024-07-12 23:27:42 +0900

    nav hover

--- fila_web/src/components/section/Header.jsx
+++ fila_web/src/components/section/Header.jsx
@@ -3,7 +3,43 @@ import React from 'react'
 const Header = () => {
     return (
         <header id='header' role='banner'>
-            header
+            <a>
+                <img src='../assets/img/HeaderLogo.svg' className='HeaderLogo'></img>
+            </a>
+            
+            <nav className='header__menu'>
+                <ul className='menu'>
+                    <li className='active'>
+                        <a href='/women'>
+                            WOMEN
+                            <img src="../img/image9.svg" class="dropdown-image"></img>
+                        </a>
+                    </li>
+                    <li>
+                        <a href='/men'>
+                            MEN
+                            <img src="../img/image9.svg" class="dropdown-image"></img>
+                        </a>
+                    </li>
+                    <li>
+                        <a href='/kids'>
+                            KIDS
+                            <img src="../img/image9.svg" class="dropdown-image"></img>
+                        </a>
+                    </li>
+                    <li>
+                        <a href='/tennis'>
+                            TENNIS
+                        </a>
+                    </li>
+                    <li>
+                        <a href='/brand'>
+                            BRAND
+                        </a>
+                    </li>
+                </ul>
+
+            </nav>
         </header>
     )
 }
