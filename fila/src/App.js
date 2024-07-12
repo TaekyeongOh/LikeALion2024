8270c7eefae0b689efadecc1d556691a7abf86ca
TaekyeongOhMe <161999984+TaekyeongOh@users.noreply.github.com>
2024-07-12 20:34:51 +0900

    CSS 설정하기

---
+++ fila/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home'
+import Women from './pages/Women'
+import Men from './pages/Men'
+import Kids from './pages/Kids'
+import Tennis from './pages/Tennis'
+import Brand from './pages/Brand'
+import Not from './pages/Not'
+
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/women' element={<Women />} />
+      <Route path='/men' element={<Men />} />
+      <Route path='/kids' element={<Kids />} />
+      <Route path='/tennis' element={<Tennis />} />
+      <Route path='/brand' element={<Brand />} />
+      <Route path='*' element={<Not />} />
+    </Routes>
+  )
+}
+
+export default App
